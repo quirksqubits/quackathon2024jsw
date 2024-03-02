072094951cd59910001c00d3c2d6f921e34a2809
quirksqubits <quirksqubits@gmail.com>
2024-03-02 13:11:12 -0500

    commit this#

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
